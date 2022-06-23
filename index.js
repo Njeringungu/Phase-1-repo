cdbe930932c5e56955be91d337cbad3b401c5cee
Janet Njeri Ngungu <janetnjerin89@gmail.com>
2022-06-23 20:27:29 +0300

    adding my project

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 
-const Url = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic"
-document.addEventListener("DOMContentLoaded", function(){
-    fetch(Url)
-    .then(resp => resp.json)
-    .then(data => data)
+
+fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic')
+
+    .then(response => response.json())
+    .then(data => {
+
+      let theCocktails = data["drinks"].slice(0,8)
+      theCocktails.forEach(cocktail => {
+        const id = cocktail.idDrink
+        const name = cocktail.strDrink
+        const imageUrl = cocktail.strDrinkThumb
+
+
 })
+
+ });
+   
     
+
